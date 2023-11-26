eabc7e4488d526b919b7f2b44614a31b8dea6941
philly12399 <philly12399@gmail.com>
2023-11-27 03:23:14 +0800

    writebbox

--- bin2pcd.py
+++ bin2pcd.py
@@ -33,7 +33,28 @@ def convert(binfolder, pcdfolder, convert_number):
 
             # Save to whatever format you like
             o3d.io.write_point_cloud(velodyne_file_new, o3d_pcd,write_ascii=True)            
-        
+def check(binfolder, pcdfolder, convert_number):
+    if(convert_number == -1):
+        convert_number = 99999999
+    file_list = os.listdir(binfolder)
+    file_list.sort()
+    count = 0
+    if os.path.exists(pcdfolder):
+        pass
+    else:
+        os.makedirs(pcdfolder)
+    convert_number = min(convert_number,len(file_list))
+    print("Total: ", len(file_list)," bins")
+    print("Convert: ", convert_number, " bins")
+    file_list = file_list[:convert_number]
+    for (count,file) in  enumerate(tqdm(file_list, desc="Converting")):     
+        (filename, extension) = os.path.splitext(file)
+        if extension == ".bin" and count < convert_number:
+            velodyne_file = os.path.join(binfolder, filename) + '.bin'
+            velodyne_file_new = os.path.join(pcdfolder, filename) + '.pcd'
+            bin_pcd = np.fromfile(velodyne_file, dtype=np.float32)
+            print(bin_pcd.shape)
+            # Reshape and drop reflection values
         
 
 
