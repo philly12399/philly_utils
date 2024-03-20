9fcb6df2809d52fe92b30bd6d1cc5590ab29328e
philly12399 <philly12399@gmail.com>
2024-03-21 03:31:02 +0800

    plot add more shape

--- plot.py
+++ plot.py
@@ -5,12 +5,12 @@ class Plot():
     def __init__(self):
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        ax.set_xlim(-5, 5)  # Set the x-axis limits
-        ax.set_ylim(-5, 5)  # Set the y-axis limits
-        ax.set_zlim(-5, 5)  # Set the z-axis limits
-        # ax.set_xlim(-20, 40)  # Set the x-axis limits
-        # ax.set_ylim(-30, 30)  # Set the y-axis limits
-        # ax.set_zlim(-5, 55)  # Set the z-axis limits
+        # ax.set_xlim(-5, 5)  # Set the x-axis limits
+        # ax.set_ylim(-5, 5)  # Set the y-axis limits
+        # ax.set_zlim(-5, 5)  # Set the z-axis limits
+        ax.set_xlim(-20, 40)  # Set the x-axis limits
+        ax.set_ylim(-30, 30)  # Set the y-axis limits
+        ax.set_zlim(-5, 55)  # Set the z-axis limits
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
@@ -26,11 +26,59 @@ class Plot():
             self.ax.plot([vertices[j][0] for j in i+[i[0]]],
                     [vertices[j][1] for j in i+[i[0]]],
                     [vertices[j][2] for j in i+[i[0]]])
+    def draw_bbox_dict(self,bbox): # Object3D
+        R = self.roty(bbox['roty'])
+        l, w, h = bbox['l'], bbox['w'], bbox['h']
+        x = bbox['x']
+        y = bbox['y']
+        z = bbox['z']
+        # 3d bounding box corners
+        x_corners = [l/2,l/2,-l/2,-l/2,l/2,l/2,-l/2,-l/2];
+        y_corners = [w/2,-w/2,-w/2,w/2,w/2,-w/2,-w/2,w/2];
+        z_corners = [h/2,h/2,h/2,h/2,-h/2,-h/2,-h/2,-h/2];
+        corners_3d = np.dot(R, np.vstack([x_corners, y_corners, z_corners]))
+        corners_3d[0,:] = corners_3d[0,:] + x
+        corners_3d[1,:] = corners_3d[1,:] + y
+        corners_3d[2,:] = corners_3d[2,:] + z
+        corners_3d = np.transpose(corners_3d)
+        self.draw_cube(corners_3d)
+        return
+    
+    def draw_bbox_obj(self,bbox): #AB3DMOT OBJ
+        R = self.roty(bbox.ry)
+        l, w, h = bbox.l, bbox.w, bbox.h
+        x = bbox.x
+        y = bbox.y
+        z = bbox.z
+        # 3d bounding box corners
+        x_corners = [l/2,l/2,-l/2,-l/2,l/2,l/2,-l/2,-l/2];
+        y_corners = [w/2,-w/2,-w/2,w/2,w/2,-w/2,-w/2,w/2];
+        z_corners = [h/2,h/2,h/2,h/2,-h/2,-h/2,-h/2,-h/2];
+        corners_3d = np.dot(R, np.vstack([x_corners, y_corners, z_corners]))
+        corners_3d[0,:] = corners_3d[0,:] + x
+        corners_3d[1,:] = corners_3d[1,:] + y
+        corners_3d[2,:] = corners_3d[2,:] + z
+        corners_3d = np.transpose(corners_3d)
+        self.draw_cube(corners_3d)
+        return
+    
     def draw_point(self,point):
         self.ax.scatter(point[0], point[1], point[2], c='r', marker='o', s=1)
         # 設置坐標軸標簽     
+        
     def show(self):
         plt.show()
+        
+    def save(self,save_path="./output.png"):
+        plt.savefig(save_path)
+        
+    def roty(self,t):
+        """Rotation about the z-axis."""
+        c = np.cos(t)
+        s = np.sin(t)
+        return np.array([[c,  -s,  0],
+                        [s,  c,  0],
+                        [0, 0,  1]])
 
 
 # cube_vertices = np.array([
