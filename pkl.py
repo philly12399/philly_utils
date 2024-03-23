eb9469912e91cc6902d36c9c84d8755a8b2f5fc7
philly12399 <philly12399@gmail.com>
2024-03-23 15:35:05 +0800

    shapenet convert

--- pkl.py
+++ pkl.py
@@ -1,21 +1,46 @@
 import pickle
-# a="/home/philly12399/philly_data/kitti/kitti_dbinfos_train.pkl"
-# with open(a, 'rb') as file:
-#     data = pickle.load(file)
-#     print(data['Car'][:10])
-
-    
 import numpy as np
+import os
+import open3d as o3d
+import plot
+def read_pkl(path): 
+    # path="/home/philly12399/thesis/philly_utils/output/kitti_dbinfos_train.pkl"
+    with open(path, 'rb') as file:
+        data = pickle.load(file)
+        # print(data['Car'][:10])
+        # print(data[0])
+        return data
+
 
 def read_gt_points_from_bin(bin_file):
     gt_points = np.fromfile(bin_file, dtype=np.float32).reshape(-1, 4)
     return gt_points
 
-# 示例用法
-import os
-p = "/home/philly12399/philly_data/kitti/gt_database/"
-p="./output/"
-for b in os.listdir(p)[:10]:
-    print(b)
-    gt_points = read_gt_points_from_bin(p+b)
-    print(gt_points.shape)
+def npy2pcd(npy):
+    xyz = np.load(npy)
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(xyz)
+    return pcd
+
+
+def shapenet2pcd():
+    classname='car'
+    path="/home/philly12399/philly_data/ShapeNet55-34/class/"+classname
+    outpath = "/home/philly12399/philly_data/ShapeNet55-34/class_pcd/"+classname
+    os.system(f"mkdir -p {outpath}")
+    idmap={}
+    num = 10
+    
+    for i,p in enumerate(sorted(os.listdir(path))):
+        if(i>=num):
+            break
+        print(i,p)
+        pcd = npy2pcd(os.path.join(path,p))
+        filename=str(i).zfill(6)+".pcd"
+        idmap[filename] = p
+        o3d.io.write_point_cloud(os.path.join(outpath,filename), pcd, write_ascii = True)
+        
+    with open(os.path.join(outpath, f'../{classname}_map.pkl'), 'wb') as file:
+        pickle.dump(idmap, file)
+if __name__ == "__main__":
+    shapenet2pcd()
