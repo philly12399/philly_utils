8168612eb0ff11739d475df48d54d5d8369f1db4
philly12399 <philly12399@gmail.com>
2024-04-17 07:21:14 +0800

    NDT with PDF sample

--- NDT.py
+++ NDT.py
@@ -26,7 +26,14 @@ def test(track_path='./output_bytrackid/car_mark_all_rotxy'):
             pcd_path=os.path.join(tpath, str(f).zfill(6)+'_dense_points.txt')
             track[t].append(pcd_path)
         # one track only
-        # break
+        break
+    
+    VOXEL_SIZE = 1.0
+    DENSITY = 1000
+    NUM_SAMPLES = int((VOXEL_SIZE**3)*DENSITY)
+    PDF_FLAG=True
+    if(PDF_FLAG):
+        print(f"Sample {NUM_SAMPLES} points per voxel")
     
     for i, tid in enumerate(track):
         t_info = info[tid]
@@ -35,31 +42,40 @@ def test(track_path='./output_bytrackid/car_mark_all_rotxy'):
         allpts=[]
         for fi, f in enumerate(frames):
             pcd = read_vis_points(f)
-          
+            vis.create_window()
             bbox = t_info[fi]['obj']['box3d']
             bbox['x'],bbox['y'],bbox['z'] = 0,0,0
-            voxel = voxelize(pcd,bbox,0.5)
-            drawbox(vis,bbox,color = [1,0,0])
+            
+            voxel = voxelize(pcd,bbox,VOXEL_SIZE,overlap=True)
+
+            
+            # # drawbox(vis,bbox,color = [1,0,0])
             p_mean = []
             pts = []
-            
-            # vv= [voxel[vi] for vi in [0, 1,40,5] ]
+            pdf = []
+
             for v in voxel:
                 # drawbox(vis,v)
                 if(len(v["pts"]) > 0):
                     pts += v["pts"].tolist()
                 if(v['mean'] is not None):
-                    p_mean.append(v['mean'])   
-            allpts+=p_mean
+                    p_mean.append(v['mean'])  
+                    #random sample     
+                    if(PDF_FLAG):                                    
+                        samples = np.random.multivariate_normal(v['mean'], v['cov'], NUM_SAMPLES)
+                        pdf.append(samples.tolist())
 
-            
-            
-            
+            allpts+=p_mean 
+            if(PDF_FLAG):                      
+                pdf = np.array(pdf).reshape(-1,3) 
+                print(pdf.shape)
             # p = o3d.geometry.PointCloud()
-            # # p.points = o3d.utility.Vector3dVector(p_mean)
+            # p.points = o3d.utility.Vector3dVector(p_mean)
             # p.points = o3d.utility.Vector3dVector(np.array(pts))
+            # p.points = o3d.utility.Vector3dVector( pdf )
+            
             # vis.add_geometry(p)
-            # # vis.get_render_option().background_color = np.asarray([0, 0, 0])
+            # vis.get_render_option().background_color = np.asarray([0, 0, 0])
             # vis.run()
             # vis.destroy_window()
         # print(len(allpts))
@@ -79,14 +95,17 @@ def test(track_path='./output_bytrackid/car_mark_all_rotxy'):
 def voxelize(pcd, box, voxel_size=0.3, overlap=True):
     l, w, h = box['l'], box['w'], box['h']
     voxel = []    
-    
-    # ln,wn,hn = int(l/voxel_size),int(w/voxel_size),int(h/voxel_size)
+    origin = 0
+    # origin = voxel_size/2
     if(overlap):
         stride = voxel_size/2
         neighbor = [-1,0,1]
+        scalar=8
     else:
         stride = voxel_size
         neighbor = [0]
+        scalar=1
+        
     ln, wn, hn = math.ceil(l / (stride))+1, math.ceil(w / (stride))+1, math.ceil(h / (stride))+1
     voxel = []
     for i in range(0, ln):
@@ -94,9 +113,9 @@ def voxelize(pcd, box, voxel_size=0.3, overlap=True):
             for k in range(0, hn):
                 # 起點voxel中心在原點,如果想讓左下在原點就要+voxel_size/2,並把ln,wn,hn -1
                 voxel.append({
-                    'x': i * (stride) - l / 2 ,
-                    'y': j * (stride) - w / 2 ,
-                    'z': k * (stride) - h / 2 ,
+                    'x': i * (stride) - l / 2 + origin,
+                    'y': j * (stride) - w / 2 + origin,
+                    'z': k * (stride) - h / 2 + origin,
                     'l': voxel_size,
                     'w': voxel_size,
                     'h': voxel_size,
@@ -104,21 +123,22 @@ def voxelize(pcd, box, voxel_size=0.3, overlap=True):
                 })
    
     #regular
+    incnt = 0
     for p in pcd:
-        i,j,k = int((p[0]+l/2)/(voxel_size/2)), int((p[1]+w/2)/(voxel_size/2)), int((p[2]+h/2)/(voxel_size/2))    
+        i,j,k = int((p[0]+l/2+ (voxel_size/2-origin))/(stride)), int((p[1]+w/2+ (voxel_size/2-origin))/(stride)), int((p[2]+h/2+ (voxel_size/2-origin))/(stride))   
         for di in neighbor:
             for dj in neighbor:
                 for dk in neighbor:                        
                     idx = (i+di)*wn*hn + (j+dj)*hn + (k+dk)
                     if(idx >= 0 and idx < len(voxel)):
                         if(in_bbox(p,voxel[idx])):                                
-                            voxel[idx]['pts'].append(p)       
-                                             
+                            voxel[idx]['pts'].append(p)
+                            incnt += 1  
+                           
+    assert incnt == scalar*len(pcd)
     # #statistic
-    for vi,v in enumerate(voxel):
-        # v["pts"] = np.array(results[vi])       
-        v["pts"] = np.array(v["pts"])        
-         
+    for vi,v in enumerate(voxel):     
+        v["pts"] = np.array(v["pts"])          
         if(len(v["pts"]) > 1):
             v['mean'] = np.mean(v["pts"],0)
             v['var'] =  np.var(v["pts"],0)
