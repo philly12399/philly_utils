97dc1e43f34ac3587901dbe76b3b8c2e5a2bf6f0
philly <philly12399@gmail.com>
2024-08-12 23:59:13 +0800

    modify merge detection

--- file_convert_script/merge_kitti_detect.py
+++ file_convert_script/merge_kitti_detect.py
@@ -9,14 +9,14 @@ import json
     "--inpath",
     "-i",
     type=str,
-    default="/home/philly12399/philly_data/KT_output/0001",
+    default="/home/philly12399/philly_ssd/KITTI_tracking/training/kitti_detection/second_iou/training",
     help="Path of labels",
 )
 @click.option(
     "--outpath",
     "-o",
     type=str,
-    default="/home/philly12399/philly_data/KT_output",
+    default="/home/philly12399/philly_ssd/KITTI_tracking/training/merged_det/second_iou",
     help="Path of output",
 )
 @click.option(
@@ -27,7 +27,10 @@ import json
     help="path you want to align",
 )
 def main(inpath, outpath,exp):
-    merge_dets(inpath, outpath, exp)
+    indirs=sorted(os.listdir(inpath))
+    for d in indirs:
+        in1=os.path.join(inpath,d)
+        merge_dets(in1, outpath, d)
 
 
 # merge kitti track label {i}.txt in same sequence to one label.txt
@@ -37,16 +40,24 @@ def merge_dets(inpath, outpath,exp):
     merged=""
     f=0
     buf=""
+    clsdict={}
     print(f"merge {inpath} to {outpath}/{exp}.txt")
-    
     for i in indir:
         with open(os.path.join(inpath,i)) as fp:
             for line in fp:
+                col=line.split(" ")
+                if(len(col)<16):
+                    continue
+                c=line.split(" ")[0]
+                if(c not in clsdict):
+                    clsdict[c]=0
+                clsdict[c]+=1                
                 s = f"{f} -1 {line}"
                 buf+=s
         f += 1
     os.system("mkdir -p {}".format(outpath))
     outfile = open(os.path.join(outpath,f"{exp}.txt"), "w")
+    print(clsdict)
     outfile.write(buf)
     outfile.close()
 
