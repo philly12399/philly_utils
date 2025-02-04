779d9d51d89cc286cbcabf567a1f0045ab6208ca
philly <philly12399@gmail.com>
2025-02-05 01:49:20 +0800

    demo config

--- utils/demo.sh
+++ utils/demo.sh
@@ -1,1 +1,1 @@
-python3 create_gt_db_kitti.py -l ../data/KITTI_tracking/training/merged_det/mark_det -o ../data/gt_db/demo
+python3 create_gt_db_kitti.py -l ../data/KITTI_tracking/training/gt_det_set_seq/diff3 -o ../data/gt_db/demo
